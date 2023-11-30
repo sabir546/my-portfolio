766bcc3f4ebfaebf86605802b15942ba9a6aae1a
sabir546 <mdgulamsabir9@gmail.com>
2023-11-30 19:30:54 +0530

    AWS data base is connected

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose')
-.connect('mongodb+srv://mdgulamsabir9:zV252VjkAKVMY2ZN@cluster0.dms7hk3.mongodb.net/?retryWrites=true&w=majority')
+.connect('mongodb+srv://mdgulamsabir9:r2HcZbcng675etwv@cluster0.i3jkt5d.mongodb.net/?retryWrites=true&w=majority')
 .then(()=>console.log("Db connected"))
 .catch((error)=>console.log(error))
 
